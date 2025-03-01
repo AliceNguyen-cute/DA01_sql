94354ad83bcc7bef6889883b9236ecad118bd226
AliceNguyen-cute <thuanhhoangn2005@gmail.com>
2025-03-01 20:45:16 +0700

    Delete EX1_PRA1.sql

--- EX1_PRA1.sql
+++
@@ -1,5 +0,0 @@
-/* Query a list of CITY and STATE from the STATION table.
-The STATION table is described as follows:
-where LAT_N is the northern latitude and LONG_W is the western longitude.
-*/
-select CITY, STATE from STATION;
