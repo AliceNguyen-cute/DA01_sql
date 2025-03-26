5cadbba8401367fe769ca25efcf0f3e0d0ef2928
AliceNguyen-cute <thuanhhoangn2005@gmail.com>
2025-03-27 01:00:01 +0700

    Create TEST 6.sql

---
+++ PRACTICE TEST 6/TEST 6.sql
@@ -0,0 +1,55 @@
+-- BÀI 1:
+with job_count_table as (select company_id,
+title,
+description,
+count(job_id) as job_count
+from job_listings
+GROUP BY company_id, title, description
+)
+select count(distinct company_id) as duplicate_companies
+from job_count_table 
+where job_count > 1
+-- BÀI 2:
+  
+(SELECT CATEGORY, PRODUCT,
+SUM (SPEND) AS TOTAL_SPEND
+FROM product_spend
+WHERE EXTRACT(YEAR FROM transaction_date) = 2022
+AND category = 'appliance'
+GROUP BY CATEGORY, PRODUCT
+order by TOTAL_SPEND desc
+LIMIT 2
+)
+UNION all 
+
+(SELECT CATEGORY, PRODUCT,
+SUM (SPEND) AS TOTAL_SPEND
+FROM product_spend
+WHERE EXTRACT(YEAR FROM transaction_date) = 2022
+AND category = 'electronics'
+GROUP BY CATEGORY, PRODUCT
+order by TOTAL_SPEND desc
+LIMIT 2
+)
+
+-- BÀI 3:
+  
+with count_call as (select 
+policy_holder_id,
+count(case_id)
+from callers
+group by policy_holder_id
+)
+
+select count (policy_holder_id)
+from count_call 
+ where count >=3
+
+-- BÀI 4:
+SELECT A.page_id FROM PAGES  A
+LEFT JOIN page_likes B
+ON A.page_id = B.page_id
+WHERE B.page_id IS NULL
+ORDER BY A.PAGE_ID ASC
+
+-- BÀI 5:
